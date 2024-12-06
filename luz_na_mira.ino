7b3608a87228b8cf76d113f0ee0a20e0329e0de8
samanthans <samanthannes@outlook.com>
2024-12-06 19:50:56 -0300

    Hopefully Final Version

--- luz_na_mira.ino
+++ luz_na_mira.ino
@@ -12,18 +12,28 @@ DFRobotDFPlayerMini myDFPlayer;
 
 uint16_t player1leds[5] = {4, 5, 6, 7, 8};
 uint16_t player2leds[5] = {9, 10, 11, 12, A0};
-const unsigned int btnValues[5] = {85, 39, 23, 15, 9};
+const unsigned int btn2Values[5] = {900, 450, 300, 225, 170};
+const unsigned int btn1Values[5] = {890, 420, 265, 175, 110};
 uint16_t player1btn = A3;
 uint16_t player2btn = A2;
 long randNumber;
 int step_counter = 0;
 int p1_score = 0;
 int p2_score = 0;
-int action_speed = 1000;
-int action_speed_min = 200;
+int action_speed;
+const int initial_speed = 2000;
+int action_speed_min = 500;
 uint8_t leds_cnt = 5;
 int pin_light = 0;
 int buttonNum = 1;
+bool p1a = false;
+bool p2a = false;
+unsigned long startTime;   // Stores the time when LED lights up
+unsigned long reactionTimeP1; // Time it takes for player to press button
+unsigned long reactionTimeP2;
+int scalingFactor = 400;
+int prev_pin_light;
+bool isReverse = false;
 
 uint8_t row = 1;
 // Custom character arrays (0-7 for valid CGRAM addresses)
@@ -51,7 +61,7 @@ byte barU[8][8] = {
 };
 
 
-int buttonPressed(int pin);
+int buttonPressed(uint8_t player);
 
 void setup() {
   Serial.begin(9600);
@@ -77,36 +87,7 @@ void setup() {
     Serial.println(F("2.Please insert the SD card!"));
   }
   myDFPlayer.volume(20);  //Set volume value. From 0 to 30
-
-
-  for(int i = 0, n = 5; i < n; i++)
-  {
-    digitalWrite(player1leds[i], HIGH);
-    digitalWrite(player2leds[i], HIGH);
-  }
-  delay(500);
-  lcd.setCursor(6, 1);
-  lcd.print("INICIAR");
-  while (buttonPressed(player1btn) < 0 || buttonPressed(player2btn) < 0 ){}
-
-  lcd.clear();
-
-  myDFPlayer.play(3);
-  for(int i = 0, n = 5; i < n; i++)
-  {
-    digitalWrite(player1leds[i], LOW);
-    digitalWrite(player2leds[i], LOW);
-    if ((n-i) % 2 != 0){
-      printNumber(n-i, 8);
-    } else {
-      printNumberU(n-i, 8);
-    }
-    
-    delay(900);
-  }
-  printNumber(0, 8);
-  lcd.clear();
-  lcd.print("PONTOS");
+  startGameSequence();
 }
 
 void loop() {
@@ -117,61 +98,110 @@ void loop() {
     bool step_action = false;
     if (step_counter > action_speed) {
       step_counter = 0;
+      
       step_action = true;  
-      action_speed = action_speed - round(action_speed/50);
+
+      action_speed = action_speed - round(action_speed/20);
       if (action_speed < action_speed_min) {
         action_speed = action_speed_min;
       }
-      Serial.println(action_speed);
     }
+    
   
-    if (step_action) {
-      pin_light = random(0,5);
+    if (step_action && !(p1a && p2a)) { // 
+      for (int i = 0; i < 5; i++){
+        digitalWrite(player1leds[pin_light], LOW);
+        digitalWrite(player2leds[pin_light], LOW);
+      }
+      do {
+        pin_light = random(0,5);
+      } while (prev_pin_light == pin_light);
+      prev_pin_light = pin_light;
+
       digitalWrite(player1leds[pin_light], HIGH);
       digitalWrite(player2leds[pin_light], HIGH);
+      p1a = true;
+      p2a = true;
+      startTime = millis();
+      Serial.print("Step Counter: ");
+      Serial.println(step_counter);
+      Serial.print("Action Speed: ");
+      Serial.println(action_speed);
+      Serial.print("Action Speed Min: ");
+      Serial.println(action_speed_min);
+
     }
 
-    for (int i = 0; i < 5; i++) {
-      if (buttonPressed(player1btn) == pin_light){
-        if(digitalRead(player1leds[i]) == HIGH){
-          digitalWrite(player1leds[i], LOW);
-          p1_score++;
-          lcd.setCursor(0, 1);
-          lcd.print(p1_score);
-          myDFPlayer.play(1);
-        }
-      }
+    int player1Button = buttonPressed(1);
+    int player2Button = buttonPressed(2);
+
+    if (player1Button == pin_light && p1a) {
+      digitalWrite(player1leds[pin_light], LOW);
+      reactionTimeP1 = millis() - startTime;
+      p1_score += calculatePoints(reactionTimeP1);
+      p1a = false;
+      lcd.setCursor(0, 1);
+      lcd.print(p1_score);
+      myDFPlayer.play(1);
+      printScore();
     }
-  
-    for (int i = 0; i < 5; i++) {
-      if (buttonPressed(player2btn) == pin_light){
-        if(digitalRead(player2leds[i]) == HIGH){
-          digitalWrite(player2leds[i], LOW);
-          p2_score++;
-          lcd.setCursor(0, 2);
-          lcd.print(p2_score);
-          myDFPlayer.play(1);
-        }
-      }
+
+    if (player2Button == pin_light && p2a) {
+      digitalWrite(player2leds[pin_light], LOW);
+      reactionTimeP2 = millis() - startTime;
+      p2_score += calculatePoints(reactionTimeP2);
+      p2a = false;
+      lcd.setCursor(0, 2);
+      lcd.print(p2_score);
+      myDFPlayer.play(1);
+      printScore();
     }
+    delay(1);
     
   } 
   
   else {
     if (p1_score > p2_score) {
       // Mostrar que o jogador 1 ganhou
+      digitalWrite(player2leds[pin_light], LOW);
       lcd.clear();
-      lcd.print("Jogador 1");
-      lcd.setCursor(0,2);
-      lcd.print("Vencedor!");
+      lcd.setCursor(0,1);
+      lcd.print("Fim de jogo!");
       myDFPlayer.play(4);
-    } else {
+
+      for(int j = 0; j < 7; j++){
+        for (int i = 0; i < 5; i++){
+          digitalWrite(player1leds[i], LOW);
+        }
+        delay(300);
+        for (int i = 0; i < 5; i++){
+          digitalWrite(player1leds[i], HIGH);
+        }
+        delay(700);
+      }
+      startGameSequence();
+    } 
+    else {
       // Mostrar que o jogador 2 ganhou
+      digitalWrite(player1leds[pin_light], LOW);
       lcd.clear();
-      lcd.print("Jogador 2");
-      lcd.setCursor(0,2);
-      lcd.print("Vencedor!");
+
+      lcd.setCursor(4,1);
+      lcd.print("Fim de jogo!");
       myDFPlayer.play(4);
+
+      for(int j = 0; j < 7; j++){
+        for (int i = 0; i < 5; i++){
+          digitalWrite(player2leds[i], LOW);
+        }
+        delay(300);
+        for (int i = 0; i < 5; i++){
+          digitalWrite(player2leds[i], HIGH);
+        }
+        delay(700);
+      }
+
+      startGameSequence();
     }   
   }
 }
@@ -192,24 +222,24 @@ void custom0(int col)
 void custom1(int col)
 {
   lcd.setCursor(col, row);
-  lcd.write(1);
   lcd.write(32);
+  lcd.write(1);
   lcd.write(32);
   lcd.setCursor(col, row+1);
-  lcd.write(1);
   lcd.write(32);
+  lcd.write(1);
   lcd.write(32);
 }
 void custom1u(int col)
 {
   lcd.setCursor(col, row);
   lcd.write(32);
-  lcd.write(32);
   lcd.write(0);
+  lcd.write(32);
   lcd.setCursor(col, row+1);
   lcd.write(32);
-  lcd.write(32);
   lcd.write(0);
+  lcd.write(32);
 }
 
 void custom2(int col)
@@ -408,9 +438,7 @@ void custom9u(int col)
 }
 
 void printNumber(int value, int col) {
-  for (int i = 0; i < 8; i++)
-    lcd.createChar(i, bar[i]);
-  
+  normal();
   switch(value) {
     case 0: custom0(col); break;
     case 1: custom1(col); break;
@@ -427,9 +455,7 @@ void printNumber(int value, int col) {
 }
 
 void printNumberU(int value, int col) {
-  for (int i = 0; i < 8; i++)
-    lcd.createChar(i, barU[i]);
-  
+  reverse();
   switch(value) {
     case 0: custom0(col); break;
     case 1: custom1u(col); break;
@@ -446,27 +472,144 @@ void printNumberU(int value, int col) {
 }
 
 
-int buttonPressed(int pin){
-  int reading = 0;
-  for(int i = 0; i < 20; i++){
-    reading += analogRead(pin);
+int buttonPressed(uint8_t player){
+  int value;
+  if(player == 1) {
+
+   value = analogRead(player1btn);
+            if (value > btn1Values[0] && value < 1023)
+            return 0;
+            else if (value > btn1Values[1] && value < btn1Values[0])
+            return 1;
+            else if (value > btn1Values[2] && value < btn1Values[1])
+            return 2;
+            else if (value > btn1Values[3] && value < btn1Values[2])
+            return 3;
+            else if (value > btn1Values[4] && value < btn1Values[3])
+            return 4;
+            else
+            return -1;
   }
-  reading /= 20;
 
-  int value = map(reading, 0, 1023, 0, 100);
-
-  if (value > btnValues[0] && value < 100)
-  return 0;
-  else if (value > btnValues[1] && value < btnValues[0])
-  return 1;
-  else if (value > btnValues[2] && value < btnValues[1])
-  return 2;
-  else if (value > btnValues[3] && value < btnValues[2])
-  return 3;
-  else if (value > btnValues[4] && value < btnValues[3])
-  return 4;
-  else
-  return -1;
-
+  else if (player == 2) {
+    value = analogRead(player2btn);
+            if (value > btn2Values[0] && value < 1023)
+            return 0;
+            else if (value > btn2Values[1] && value < btn2Values[0])
+            return 1;
+            else if (value > btn2Values[2] && value < btn2Values[1])
+            return 2;
+            else if (value > btn2Values[3] && value < btn2Values[2])
+            return 3;
+            else if (value > btn2Values[4] && value < btn2Values[3])
+            return 4;
+            else
+            return -1;
+  }
 }
 
+int calculatePoints(unsigned long reactionTime) {
+  int offset = 1; // Minimum guaranteed points
+  int basePoints = 2000; // Max reaction time to consider
+  if (reactionTime > basePoints) return 1;
+  return max(0, (basePoints - reactionTime) / scalingFactor + offset);
+}
+
+void pause(){
+  while (buttonPressed(1) < 0 || buttonPressed(2) < 0 ){}
+  return;
+}
+
+void startGameSequence(){
+  lcd.clear();
+  for(int i = 0, n = 5; i < n; i++)
+  {
+    digitalWrite(player1leds[i], HIGH);
+    digitalWrite(player2leds[i], HIGH);
+  }
+  delay(500);
+  lcd.setCursor(6, 0);
+  lcd.print("INICIAR");
+  lcd.setCursor(3, 2);
+  lcd.print("APERTAR BOTAO");
+  lcd.setCursor(3, 3);
+  lcd.print("EM AMBOS LADOS");
+  pause();
+
+  lcd.clear();
+
+  myDFPlayer.play(3);
+  for(int i = 0, n = 5; i < n; i++)
+  {
+    digitalWrite(player1leds[i], LOW);
+    digitalWrite(player2leds[i], LOW);
+    if ((n-i) % 2 != 0){
+      printNumber(n-i, 8);
+    } else {
+      printNumberU(n-i, 8);
+    }
+    
+    delay(900);
+  }
+  printNumber(0, 8);
+  delay(900);
+  lcd.clear();
+  p1_score = 0;
+  p2_score = 0;
+  p1a = false;
+  p2a = false;
+  step_counter = 0;
+  action_speed = initial_speed;
+  lcd.print("PONTOS");
+  return;
+}
+
+void printScore(){
+  Serial.println(p1_score);
+  Serial.println(p1_score);
+  int dezena, unidade;
+  uint8_t op;
+  if (p1_score > p2_score){
+    op = 1;
+  }
+  else if (p2_score > p1_score){
+    op = 2;
+  }
+  else {
+    op = 1;
+  }
+  switch(op){
+    case 1:
+      dezena = p1_score / 10;
+      unidade = p1_score % 10;
+      if (dezena > 0)
+      printNumberU(dezena, 16);
+      printNumberU(unidade, 12);
+      break;
+
+    case 2:
+      dezena = p2_score / 10;
+      unidade = p2_score % 10;
+      if (dezena > 0)
+      printNumber(dezena, 12);
+      printNumber(unidade, 16);
+      break;
+  }
+  return;
+}
+
+void reverse(){
+  if (!isReverse){
+    for (int i = 0; i < 8; i++)
+        lcd.createChar(i, barU[i]); 
+  }
+  isReverse = true;
+}
+
+void normal(){
+  if (isReverse){
+    for (int i = 0; i < 8; i++)
+      lcd.createChar(i, bar[i]);
+  }
+  isReverse = false;
+}
